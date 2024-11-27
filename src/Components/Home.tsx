56faf8db2d33f6ada79ae0bf155d1d0178644cbb
AndressaSales <andressaccs2001@gmail.com>
2024-11-27 14:51:42 -0300

    finalizando a navbar e o home

---
+++ src/Components/Home.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+
+export default function Home(){
+    return(
+        <>
+            <Navbar />
+            <div className="container-home">
+                <h1>Pizza</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit</p>
+            </div>
+        </>
+    )
+}
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,85 @@
+import logo from "../assets/logo.png";
+import InfoIcon from "@mui/icons-material/Info";
+import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
+import { Box, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { useState } from "react";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import { PiChefHatDuotone } from "react-icons/pi"
+
+
+export default function Navbar(){
+
+    const [open, setOpen] = useState<boolean>(false)
+
+    return(
+        <nav className="navbar">
+
+            <div>
+                <img src={logo} alt="" className="logo" />
+            </div>
+
+            <div className="navbar-link">
+                <a className="link" href="#">Sobre</a>
+                <a className="link" href="#">Serviços</a>
+                <a className="link" href="#">Chefs</a>
+                <a className="link" href="#">Menu</a>
+            </div>
+
+            <div className="menu">
+                <HiOutlineBars3 color="#fff" onClick={() => setOpen(true)} />
+            </div>
+            <Drawer open={open} onClose={() => setOpen(false)} anchor="right">
+                <Box
+                    sx={{width: 250}}
+                    role="presentation"
+                    onClick={() => setOpen(false)}
+                    onKeyDown={() => setOpen(false)}
+                >
+                    <List>
+
+                        <ListItem>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <InfoIcon sx={{fontSize:30}} />
+                                </ListItemIcon>
+                                <ListItemText>
+                                    <p className="list">Sobre</p>
+                                </ListItemText>
+                            </ListItemButton>
+                        </ListItem>
+                        
+                        <ListItem>
+                            <ListItemButton>
+                                <ListItemIcon>
+
+                                </ListItemIcon>
+                                 <ListItemText><p className="list">Serviços</p>
+                                 </ListItemText>
+                            </ListItemButton>
+                        </ListItem>
+
+                        <ListItem>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <PiChefHatDuotone className="list" />
+                                </ListItemIcon>
+                                 <ListItemText><p className="list">Chefs</p></ListItemText>
+                            </ListItemButton>
+                        </ListItem>
+
+                        
+                        <ListItem>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <RestaurantMenuIcon sx={{ fontSize: 30 }} />
+                                </ListItemIcon>
+                                 <ListItemText><p className="list">Menu</p></ListItemText>
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
+                    <Divider/>
+                </Box>
+            </Drawer>
+        </nav>
+    )
+}
